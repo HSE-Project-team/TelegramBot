793e39f7da8c088e4b46f2d11b05ae31af5c2332
Ilia Kholopkin <iliagold@icloud.com>
2024-03-17 01:05:46 +0300

    Dynamic timing button appearance and code style improvements

--- Site/script.js
+++ Site/script.js
@@ -5,7 +5,7 @@ class ItemFromCatalog {
     constructor(item_img, item_name, item_cost) {
         this.item_img = item_img;
         this.item_name = item_name;
-        this.item_cost = item_cost + " ₽";
+        this.item_cost = item_cost;
     }
 }
 
@@ -20,19 +20,19 @@ class Catalog {
 }
 
 class Order {
-    order_items = new Array(catalog.size + 1).fill(document.createElement("label"));
+    user_order = new Map();
 
     order_cost = 0;
 
     generate_data_for_send() {
-        let answer_string = "";
-        for (let i = 1; i < this.order_items.length; ++i) {
-            answer_string += `item_id=${i}, item_counter=${this.order_items[i].textContent}`
-            answer_string += ", "
+        let answer_string = "Ваш заказ:\n";
+        let item_counter = 1;
+        for (let key of order.user_order.keys()) {
+            answer_string += `${item_counter++}) ${catalog.Items[key].item_name} (${order.user_order.get(key)} шт.) – ${catalog.Items[key].item_cost}\n`;
         }
-        answer_string += `order_cost=${this.order_cost}, `;
-        answer_string += `order_time=${now_time.get_time()}`
-        return answer_string;
+        answer_string += `\nСумма заказа: ${order.order_cost}`;
+        console.log(answer_string);
+        return answer_string
     }
 }
 
@@ -74,7 +74,7 @@ class Time {
 
 catalog = new Catalog();
 
-document.getElementById("shopping_cart").style.display = "none";
+document.querySelector(".shopping_cart").classList.add("hidden");
 
 catalog.addItem("Dish1.png", "Пельмени сибирские", "100");
 catalog.addItem("Dish1.png", "Окрошка настоящая", "200");
@@ -88,6 +88,7 @@ catalog.addItem("Dish1.png", "Нежное сливочное мороженое
 order = new Order();
 
 let graphicCatalogItems = new Array(catalog.size + 1);
+let graphicCatalogItemCounter = new Array(catalog.size + 1).fill(document.createElement("label"));
 
 for (let i = 1; i < catalog.size + 1; ++i) {
     graphicCatalogItems[i] = document.createElement("div");
@@ -104,7 +105,7 @@ for (let i = 1; i < catalog.size + 1; ++i) {
 
     graphicCatalogItems[i].item_cost = document.createElement("div");
     graphicCatalogItems[i].item_cost.className = "item_cost";
-    graphicCatalogItems[i].item_cost.textContent = catalog.Items[i].item_cost;
+    graphicCatalogItems[i].item_cost.textContent = catalog.Items[i].item_cost + " ₽";
 
     graphicCatalogItems[i].item_btn = document.createElement("button");
     graphicCatalogItems[i].item_btn.className = "btn_add";
@@ -113,7 +114,7 @@ for (let i = 1; i < catalog.size + 1; ++i) {
     graphicCatalogItems[i].add_remove_figures = document.createElement("figure");
     graphicCatalogItems[i].add_remove_figures.className = "add_remove_figure";
 
-    document.getElementById("items").appendChild(graphicCatalogItems[i]);
+    document.querySelector(".items").appendChild(graphicCatalogItems[i]);
     graphicCatalogItems[i].appendChild(graphicCatalogItems[i].item_img);
     graphicCatalogItems[i].appendChild(graphicCatalogItems[i].item_name);
     graphicCatalogItems[i].appendChild(graphicCatalogItems[i].item_cost);
@@ -123,7 +124,9 @@ for (let i = 1; i < catalog.size + 1; ++i) {
 
 let choose_time_btn = document.createElement("button");
 choose_time_btn.className = "choose_time_btn";
-document.getElementById("items").appendChild(choose_time_btn);
+choose_time_btn.classList.add("hidden");
+document.querySelector(".container").classList.remove("bottom_container_margin");
+document.querySelector(".items").appendChild(choose_time_btn);
 choose_time_btn.textContent = "Посмотреть заказ";
 
 let time_slider = document.createElement("input");
@@ -133,52 +136,60 @@ time_slider.min = "0";
 time_slider.max = "60";
 time_slider.value = "0";
 time_slider.className = "time_slider";
-document.getElementById("shopping_cart").appendChild(time_slider);
-time_slider.style.display = "none";
+document.querySelector(".shopping_cart").appendChild(time_slider);
+time_slider.classList.add("hidden");
 
 
 let choose_time_label = document.createElement("label");
 choose_time_label.className = "choose_time_label";
-document.getElementById("shopping_cart").appendChild(choose_time_label);
-choose_time_label.style.display = "none";
+document.querySelector(".shopping_cart").appendChild(choose_time_label);
+choose_time_label.classList.add("hidden");
 
 let cansel_choose_time_btn = document.createElement("button");
 cansel_choose_time_btn.className = "cansel_choose_time_btn";
-document.getElementById("shopping_cart").appendChild(cansel_choose_time_btn);
+document.querySelector(".shopping_cart").appendChild(cansel_choose_time_btn);
 cansel_choose_time_btn.textContent = "×";
-cansel_choose_time_btn.style.display = "none";
+cansel_choose_time_btn.classList.add("hidden");
+document.querySelector(".container").classList.remove("bottom_container_margin");
+
 
 let now_time = new Time();
 choose_time_label.textContent = now_time.get_now_time();
 
 let checkout_btn = document.createElement("button");
 checkout_btn.className = "checkout_btn";
-document.getElementById("shopping_cart").appendChild(checkout_btn);
+document.querySelector(".shopping_cart").appendChild(checkout_btn);
 checkout_btn.textContent = "Заказать к " + now_time.get_time();
-checkout_btn.style.display = "none";
+checkout_btn.classList.add("hidden");
 
 checkout_btn.addEventListener("click", () => {
-    console.log(order.generate_data_for_send());
     tg.sendData(order.generate_data_for_send());
 });
 
 choose_time_btn.addEventListener("click", () => {
-    document.getElementById("items").style.display = "none";
-    document.getElementById("shopping_cart").style.display = "inline-block";
-    document.getElementById("shopping_cart").style.backgroundColor = "red";
+    document.querySelector(".items").classList.add("hidden");
+    document.querySelector(".items").classList.remove("items");
+    choose_time_btn.classList.add("hidden");
+    document.querySelector(".container").classList.remove("bottom_container_margin");
+    document.querySelector(".shopping_cart").classList.remove("hidden");
 
     let back_btn = tg.BackButton;
     back_btn.show();
     back_btn.onClick(() => {
-        document.getElementById("shopping_cart").style.display = "none";
-        document.getElementById("items").style.display = "grid";
+        document.querySelector(".shopping_cart").classList.add("hidden");
+        document.querySelector(".items").classList.remove("hidden");
+        document.querySelector(".items").classList.add("items");
+        choose_time_btn.classList.remove("hidden");
+        document.querySelector(".container").classList.add("bottom_container_margin");
         back_btn.hide();
     });
 
-    time_slider.style.display = "inline-block";
-    choose_time_label.style.display = "inline-block";
-    checkout_btn.style.display = "inline-block";
-    cansel_choose_time_btn.style.display = "inline-block";
+
+    time_slider.classList.remove("hidden");
+    choose_time_label.classList.remove("hidden");
+    checkout_btn.classList.remove("hidden");
+    cansel_choose_time_btn.classList.remove("hidden");
+    document.querySelector(".container").classList.add("bottom_container_margin");
 
     time_slider.addEventListener("input", () => {
         now_time.reset_time();
@@ -186,65 +197,71 @@ choose_time_btn.addEventListener("click", () => {
         choose_time_label.textContent = now_time.get_time();
         checkout_btn.textContent = "Заказать к " + now_time.get_time();
     });
-
 });
 
 cansel_choose_time_btn.addEventListener("click", () => {
-    time_slider.style.display = "none";
-    choose_time_label.style.display = "none";
-    cansel_choose_time_btn.style.display = "none";
-    checkout_btn.style.display = "none";
-    choose_time_btn.style.display = "inline-block";
-
+    time_slider.classList.add("hidden");
+    choose_time_label.classList.add("hidden");
+    cansel_choose_time_btn.classList.add("hidden");
+    document.querySelector(".container").classList.remove("bottom_container_margin");
+    checkout_btn.classList.add("hidden");
+    choose_time_btn.classList.remove("hidden");
+    document.querySelector(".container").classList.add("bottom_container_margin");
 })
 
 
 for (let i = 1; i < catalog.size + 1; ++i) {
     graphicCatalogItems[i].item_btn.addEventListener("click", () => {
-        order.order_cost += parseInt(graphicCatalogItems[i].item_cost.textContent);
+        order.user_order.set(i, 1);
+        choose_time_btn.classList.remove("hidden");
+        document.querySelector(".container").classList.add("bottom_container_margin");
 
-        graphicCatalogItems[i].item_btn.style.display = "none";
+        order.order_cost += +catalog.Items[i].item_cost;
+
+        graphicCatalogItems[i].item_btn.classList.add("hidden");
 
         graphicCatalogItems[i].minus_btn = document.createElement("button");
         graphicCatalogItems[i].minus_btn.textContent = "-";
         graphicCatalogItems[i].minus_btn.className = "btn_minus"
 
-        order.order_items[i] = document.createElement("label");
-        order.order_items[i].textContent = "1";
-        order.order_items[i].className = "order_item_label";
+        graphicCatalogItemCounter[i] = document.createElement("label");
+        graphicCatalogItemCounter[i].textContent = order.user_order.get(i);
+        graphicCatalogItemCounter[i].className = "order_item_label";
 
         graphicCatalogItems[i].plus_btn = document.createElement("button");
         graphicCatalogItems[i].plus_btn.textContent = "+";
         graphicCatalogItems[i].plus_btn.className = "btn_plus"
 
         graphicCatalogItems[i].add_remove_figures.appendChild(graphicCatalogItems[i].minus_btn);
-        graphicCatalogItems[i].add_remove_figures.appendChild(order.order_items[i]);
+        graphicCatalogItems[i].add_remove_figures.appendChild(graphicCatalogItemCounter[i]);
         graphicCatalogItems[i].add_remove_figures.appendChild(graphicCatalogItems[i].plus_btn);
 
         graphicCatalogItems[i].plus_btn.addEventListener("click", () => {
-            let new_number = +(order.order_items[i].textContent);
-            new_number += 1;
-            order.order_cost += parseInt(graphicCatalogItems[i].item_cost.textContent);
-            order.order_items[i].textContent = new_number;
-            console.log(order.generate_data_for_send());
+            let new_number = order.user_order.get(i) + 1;
+            order.user_order.set(i, new_number);
+            order.order_cost += +catalog.Items[i].item_cost;
+            graphicCatalogItemCounter[i].textContent = new_number;
         });
 
         graphicCatalogItems[i].minus_btn.addEventListener("click", () => {
-            let new_number = +(order.order_items[i].textContent);
+            let new_number = order.user_order.get(i);
             if (new_number >= 2) {
-                new_number -= 1;
-                order.order_items[i].textContent = new_number + "";
+                new_number--;
+                order.user_order.set(i, new_number);
+                graphicCatalogItemCounter[i].textContent = new_number;
             } else {
-                graphicCatalogItems[i].item_btn.style.display = "inline-block";
-                graphicCatalogItems[i].minus_btn.style.display = "none";
-                graphicCatalogItems[i].plus_btn.style.display = "none";
-                order.order_items[i].style.display = "none";
-                order.order_items[i].textContent = "0";
+                order.user_order.delete(i);
+                if (order.user_order.size === 0) {
+                    choose_time_btn.classList.add("hidden");
+                    document.querySelector(".container").classList.remove("bottom_container_margin");
+                }
+                graphicCatalogItems[i].item_btn.classList.remove("hidden");
+                graphicCatalogItems[i].minus_btn.classList.add("hidden");
+                graphicCatalogItems[i].plus_btn.classList.add("hidden");
+                graphicCatalogItemCounter[i].classList.add("hidden");
+                graphicCatalogItemCounter[i].textContent = "0";
             }
-            order.order_cost -= parseInt(graphicCatalogItems[i].item_cost.textContent);
-            console.log(order.generate_data_for_send());
+            order.order_cost -= +catalog.Items[i].item_cost;
         });
-
-        console.log(order.generate_data_for_send());
     })
 }
