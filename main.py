732ba7d62ca98db573a2ab86fbfe306dfc5c29bb
Ilia Kholopkin <iliagold@icloud.com>
2024-03-20 21:26:05 +0300

    Receiving json catalog and sending json order

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from aiogram import Bot, Dispatcher, types, executor
 from aiogram.types.web_app_info import WebAppInfo
 from dotenv import load_dotenv
 import os
+import requests
+import json
 
 load_dotenv()
 bot = Bot(os.getenv("TESTTGTOKEN"))
@@ -24,6 +26,21 @@ orders = []
 
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
+    """
+    Telegram ---> WebApp:
+
+    markup = types.InlineKeyboardMarkup()
+    markup.add(
+        types.InlineKeyboardButton("Открыть WebApp", web_app=WebAppInfo(url="https://i2l1a1.github.io/")))
+    await message.answer("Привет!", reply_markup=markup)
+
+    WebApp ---> Telegram
+    markup = types.ReplyKeyboardMarkup()
+    markup.add(
+        types.KeyboardButton("Открыть WebApp", web_app=WebAppInfo(url="https://i2l1a1.github.io/")))
+    await message.answer("Привет!", reply_markup=markup)
+    """
+
     markup = types.ReplyKeyboardMarkup()
     markup.add(
         types.KeyboardButton("Открыть WebApp", web_app=WebAppInfo(url="https://i2l1a1.github.io/")))
@@ -48,9 +65,19 @@ async def buy_process(message: types.Message):
     first_name = message.from_user.first_name
     orders.append(create_new_order(first_name, user_id))
     print_new_order(message)
-    to_user = f"{first_name}, бот принял ваш заказ. Данные из WebApp: '{message.web_app_data.data}'. \n\nID " \
-              f"аккаунта: {user_id}."
-    await message.answer(to_user)
+
+    r = requests.post('http://httpbin.org/post', json=message.web_app_data.data)
+
+    json_from_bot = json.loads(r.json()["json"])
+    string_for_user = f"{first_name}, бот принял Ваш заказ. Вы заказали:\n"
+    for item in json_from_bot["order"]:
+        string_for_user += f"• {item[0]} ({item[1]} шт.) – {item[2]} ₽\n"
+    string_for_user += f"\nСумма заказа: {json_from_bot['cost']} ₽\n"
+    string_for_user += f"Время заказа: {json_from_bot['time']}\n"
+    string_for_user += f"ID аккаунта: {user_id}\n"
+
+    print(string_for_user)
+    await message.answer(string_for_user)
 
 
 executor.start_polling(dp)
