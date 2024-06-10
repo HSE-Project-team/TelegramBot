563fffb9b79a3971bf1dd76755409d76e94623ec
Калинин Иван <Jeck-5.ivan@yandex.ru>
2024-06-10 19:19:27 +0000

    fixed create_delievery and added get_status

--- iiko/main.py
+++ iiko/main.py
@@ -220,7 +220,6 @@ def get_creation_status(order_id):
         return None
 
 
-
 def do_test_order():
     menu = get_menu()
     if not menu:
@@ -231,9 +230,113 @@ def do_test_order():
     for product_id in list(menu.values())[:3]:
         order_items[product_id] = 0
     print(order_items)
+    id = create_delivery(order_items)
+    print(id)
 
-    id = "b0ac0368-9751-4ad0-834b-b56f3cb54793" #create_order(order_items)
-    get_creation_status(id)
+    # id = "b0ac0368-9751-4ad0-834b-b56f3cb54793" #create_order(order_items)
+    print(get_delivery_status_by_id(id))
+
+
+import requests
+from uuid import uuid4
+
+
+class Order:
+    def __init__(self, organization_id, terminal_group_id, items, phone, order_type_id=None,
+                 order_service_type="DeliveryByClient", comment=None):
+        self.organization_id = organization_id
+        self.terminal_group_id = terminal_group_id
+        self.items = items
+        self.phone = phone
+        self.order_type_id = order_type_id
+        self.order_service_type = order_service_type
+        self.comment = comment
+        self.id = str(uuid4())  # Уникальный ID заказа
+
+    def to_dict(self):
+        order_dict = {
+            "organizationId": self.organization_id,
+            "terminalGroupId": self.terminal_group_id,
+            "order": {
+                "id": self.id,
+                "phone": self.phone,
+                "items": self.items,
+            }
+        }
+        if self.order_type_id:
+            order_dict["order"]["orderTypeId"] = self.order_type_id
+        if self.order_service_type:
+            order_dict["order"]["orderServiceType"] = self.order_service_type
+        if self.comment:
+            order_dict["order"]["comment"] = self.comment
+
+        return order_dict
+
+
+def create_delivery(order_items):
+    api_login = get_api_login()
+    if not api_login:
+        return None
+
+    organization_id = get_organisation_id(api_login)
+    if not organization_id:
+        return None
+
+    terminal_group_id = get_terminal_group()
+    if not terminal_group_id:
+        return None
+
+    items = [{"productId": product_id, "type": "Product", "amount": amount, "comment": "Тестовая доставка. Не делать!"}
+             for product_id, amount in order_items.items()]
+
+    order = Order(organization_id, terminal_group_id, items, "+70000000000")
+
+    url = 'https://api-ru.iiko.services/api/1/deliveries/create'
+    headers = {
+        'Authorization': f'Bearer {get_token(api_login)}',
+        'Content-Type': 'application/json'
+    }
+    response = requests.post(url, json=order.to_dict(), headers=headers)
+    print(response.json())
+
+    if response.status_code == 200:
+        delivery_id = response.json().get('orderInfo', {}).get('id')
+        if delivery_id:
+            print(f"Доставка создана. ID доставки: {delivery_id}")
+            return delivery_id
+        else:
+            print("Ошибка: не удалось получить ID доставки из ответа.")
+            return None
+    else:
+        print(f"Ошибка при создании доставки: {response.status_code}, {response.text}")
+        return None
+
+
+def get_delivery_status_by_id(order_id):
+    api_login = get_api_login()
+    organization_id = get_organisation_id(api_login)
+    if not organization_id:
+        return None
+
+    url = 'https://api-ru.iiko.services/api/1/deliveries/by_id'
+    headers = {
+        'Authorization': f'Bearer {get_token(api_login)}',
+        'Content-Type': 'application/json'
+    }
+
+    payload = {
+        "organizationId": organization_id,
+        "orderIds": [order_id]
+    }
+
+    response = requests.post(url, json=payload, headers=headers)
+    if response.status_code == 200:
+        print(response.json())
+        status = response.json()['orders'][0]['creationStatus']
+        return status
+    else:
+        print(f"Error getting delivery by ID: {response.status_code}, {response.text}")
+        return None
 
 
 if __name__ == "__main__":
