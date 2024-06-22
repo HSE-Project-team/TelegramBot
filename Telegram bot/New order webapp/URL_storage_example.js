1a9eea63eee40074f91a3c374dfd5235752b5a83
Ilia Kholopkin <iliagold@icloud.com>
2024-06-22 15:28:57 +0300

    URL_storage_example added

---
+++ Telegram bot/New order webapp/URL_storage_example.js
@@ -0,0 +1,4 @@
+export let main_url = "your_main_url"
+export let catalog_url = `${main_url}/catalog`;
+export let free_order_time_url = `${main_url}/availableTimes`;
+export let categories_url = `${main_url}/catalog/categories`;
---
+++ Telegram bot/Show orders webapp/URL_storage_example.js
@@ -0,0 +1,2 @@
+let main_url = "your_main_url"
+export let user_url = `${main_url}/user`;
